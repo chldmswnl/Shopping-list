fdccf4f350be75305fc0dc43f5ea960710d98c8b
EUNJICHOI\은지 <chldmswnl12@gmail.com>
2021-01-05 22:07:02 +0900

    edit js file

--- main.js
+++ main.js
@@ -27,32 +27,20 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0;
+
 function createItem(text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item_row");
-
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
-
-  const span = document.createElement("span");
-  span.setAttribute("class", "item_name");
-  span.innerText = text;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("class", "item_delete");
-  deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
-  deleteBtn.addEventListener("click", () => {
-    list.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement("div");
-  itemDivider.setAttribute("class", "item_divider");
-
-  item.appendChild(span);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute("data-id", id);
+  itemRow.innerHTML = `
+        <div class="item">
+          <span class="item_name">${text}</span>
+          <button class="item_delete"><i class="fas fa-trash" data-id=${id}></i></button>
+        </div>
+        <div class="item_divider"></div>
+  `;
+  id++;
   return itemRow;
 }
 
@@ -63,5 +51,13 @@ btn.addEventListener("click", () => {
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+list.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`); // Attribute selector
+    toBeDeleted.remove();
   }
 });
